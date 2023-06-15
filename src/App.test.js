dd217e24c541afd5e1755ea6aeba3197aa7bcde0
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-06-15 17:11:50 +0200

    button test working

--- src/App.test.js
+++ src/App.test.js
@@ -7,20 +7,18 @@ afterEach(cleanup);
 
 test("Renders green tag with text AVAILABLE if the current room is available", () => {
   const roomStatus = "Available";
-  render(<RoomStatus status={roomStatus}>{roomStatus}</RoomStatus>);
+  render(<RoomStatus status={"#5AD07A"}>{roomStatus}</RoomStatus>);
 
   const button = screen.getByRole("button", { name: roomStatus });
-  console.log(button);
 
-  expect(button).toHaveStyle(`backgroundColor: rgb(90, 208, 122)`);
+  expect(button).toHaveStyle(`backgroundColor:"#5AD07A"`);
 });
 
 test("Renders red tag with text Booked if the current room is available", () => {
   const roomStatus = "";
-  render(<RoomStatus status={roomStatus}>{roomStatus}</RoomStatus>);
+  render(<RoomStatus status={"#E23428"}>{roomStatus}</RoomStatus>);
 
-  const button = screen.getByRole("button", { name: roomStatus });
-  console.log(button);
+  const buttonRed = screen.getByRole("button", { name: roomStatus });
 
-  expect(button).toHaveStyle(`backgroundColor: rgb(226, 52, 40)`);
+  expect(buttonRed).toHaveStyle(`backgroundColor:"#E23428"`);
 });
