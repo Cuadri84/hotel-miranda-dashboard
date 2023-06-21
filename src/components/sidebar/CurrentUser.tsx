cda2d2c7417b694640e400836a0d6d7b9b108bf2
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-06-21 17:14:41 +0200

    first archive changed to TS and working

--- src/components/sidebar/CurrentUser.tsx
+++ src/components/sidebar/CurrentUser.tsx
@@ -1,6 +1,5 @@
 // React & Router
 import React from "react";
-import { Link } from "react-router-dom";
 
 // Styled Components
 import { Card, UserName, UserEmail, LinkButton } from "./CurrentUserStyled";
@@ -8,17 +7,17 @@ import { Card, UserName, UserEmail, LinkButton } from "./CurrentUserStyled";
 //assets
 import UserPic from "../../assets/sidebar/yo.jpg";
 
-const CurrentUser = () => {
+function CurrentUser() {
   return (
     <Card>
       <img className="image" src={UserPic} alt="" />
       <UserName>David Cuadrillero</UserName>
       <UserEmail>https://github.com/Cuadri84</UserEmail>
       <LinkButton>
-        <Link>Edit user</Link>
+        <button>Edit user</button>
       </LinkButton>
     </Card>
   );
-};
+}
 
 export default CurrentUser;
