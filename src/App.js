a3918a9529acd6be8d916d49df1bbd1baabd878a
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-06-22 17:30:37 +0200

    decomented the protected routes

--- src/App.js
+++ src/App.js
@@ -44,22 +44,22 @@ function App() {
                 authReady ? <Dashboard /> : <Navigate replace to="/login" />
               }
             />
-            {/* <Route path="*" element={<ProtectRoute authReady={authReady} />}> */}
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="bookings/:bookingId" element={<SingleBooking />} />
-            <Route path="editBooking/:bookingId" element={<EditBooking />} />
-            <Route path="newBooking" element={<NewBooking />} />
-            <Route path="rooms" element={<Rooms />} />
-            <Route path="rooms/:roomId" element={<SingleRoom />} />
-            <Route path="editRoom/:roomId" element={<EditRoom />} />
-            <Route path="newRoom" element={<NewRoom />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="users" element={<Users />} />
-            <Route path="users/:userId" element={<SingleUser />} />
-            <Route path="editUser/:id" element={<EditUser />} />
-            <Route path="newUser" element={<NewUser />} />
-            <Route path="login" element={<Login />} />
-            {/* </Route> */}
+            <Route path="*" element={<ProtectRoute authReady={authReady} />}>
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="bookings/:bookingId" element={<SingleBooking />} />
+              <Route path="editBooking/:bookingId" element={<EditBooking />} />
+              <Route path="newBooking" element={<NewBooking />} />
+              <Route path="rooms" element={<Rooms />} />
+              <Route path="rooms/:roomId" element={<SingleRoom />} />
+              <Route path="editRoom/:roomId" element={<EditRoom />} />
+              <Route path="newRoom" element={<NewRoom />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="users" element={<Users />} />
+              <Route path="users/:userId" element={<SingleUser />} />
+              <Route path="editUser/:id" element={<EditUser />} />
+              <Route path="newUser" element={<NewUser />} />
+              <Route path="login" element={<Login />} />
+            </Route>
           </Routes>
         </div>
       </Layout>
